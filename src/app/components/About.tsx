6e8f675f75d92e3cd28c2227d2cdda33da78c739
Petr Tesar <petr.tesar@skf.com>
2024-08-11 20:01:42 +0200

    Menu links

---
+++ src/app/components/About.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Main() {
+    return (
+        <div>
+            <div className="flex justify-center items-center pt-5">
+                <h1 className="text-2xl font-semibold">About us bro</h1>
+            </div>
+            <div className="grid md:grid-cols-3 grid-cols-1 gap-3 p-5">
+            </div>
+        </div>
+    );
+}
+
+export default Main;
--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import Link from 'next/link';
 
 function Main() {
+
   return (
     <div>
       <nav className="bg-gray-950 border-gray-200">
@@ -18,19 +20,19 @@ function Main() {
           <div className="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
               <li>
-                <a href="#" className="block py-2 px-3 text-gray-100 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Home</a>
+                <Link href="#" className="block py-2 px-3 text-gray-100 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Home</Link>
               </li>
               <li>
-                <a href="#" className="block py-2 px-3 text-gray-100 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">About</a>
+                <Link href="#" className="block py-2 px-3 text-gray-100 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">About</Link>
               </li>
               <li>
-                <a href="#" className="block py-2 px-3 text-gray-100 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Services</a>
+                <Link href="#" className="block py-2 px-3 text-gray-100 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Services</Link>
               </li>
               <li>
-                <a href="#" className="block py-2 px-3 text-gray-100 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Pricing</a>
+                <Link href="#" className="block py-2 px-3 text-gray-100 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Pricing</Link>
               </li>
               <li>
-                <a href="#" className="block py-2 px-3 text-gray-100 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Contact</a>
+                <Link href="#" className="block py-2 px-3 text-gray-100 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Contact</Link>
               </li>
             </ul>
           </div>
